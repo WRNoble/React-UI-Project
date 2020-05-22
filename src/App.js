88232b700e1746c4343bb478eca9fac3dfffe9b6
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-05-21 22:03:56 -0400

    getting error about inability to close a div tag

--- src/App.js
+++ src/App.js
@@ -7,12 +7,42 @@ class App extends Component {
     super();
 
     this.state = {
-      nasa: [];
+      nasa: [],
     }
   }
 
   componentDidMount() {
     let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=8xU4o02n5gJAwNEErowpY1mfE31mtk1ybYDbf3km"
+  
+    fetch(url)
+    console.log(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          nasa: data,
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="tab1">
+        <p></p>
+      </div>
+      
+      <div className="tab2">
+        <p></p>
+      </div>
+      <div className="tab3">
+        <p></p>
+      </div>
+      <div className="tab4">
+        <p></p>
+      </div>
+      <div className="tab5">
+        <p></p>
+      </div>
+    )
   }
 }
 
